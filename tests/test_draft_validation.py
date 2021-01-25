ccc91a19adf9f57cc85e29973696c149e620645f
Igor' Vlasov <igorkram777@gmail.com>
2021-01-25 23:11:29 +0200

    After mentor's review

--- tests/test_draft_validation.py
+++ tests/test_draft_validation.py
@@ -1,32 +1,20 @@
-from selene import have
-from selene.support.shared import browser
-from selene.support.shared.jquery_style import s, ss
+from todomvc_testing.model import app
 
 
 def test_basic_management():
-    browser.open('https://todomvc4tasj.herokuapp.com/')
-    browser.wait_to(have.js_returned
-                    (True, 'return Object.keys(require.s.contexts._.defined).length === 39'))
+    app.visit()
 
-    s('#new-todo').type('a').press_enter()
-    s('#new-todo').type('b').press_enter()
-    s('#new-todo').type('c').press_enter()
-    ss('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
+    app.add('a', 'b', 'c')
+    app.should_have('a', 'b', 'c')
 
-    ss('#todo-list>li').element_by(have.exact_text('b')).double_click()
-    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit')\
-        .type(' edited').press_enter()
+    app.edit('b', 'b edited')
 
-    ss('#todo-list>li').element_by(have.exact_text('b edited'))\
-        .element('.toggle').click()
+    app.toggle('b edited')
 
-    s('#clear-completed').click()
-    ss('#todo-list>li').should(have.exact_texts('a', 'c'))
+    app.clear_completed()
+    app.should_have('a', 'c')
 
-    ss('#todo-list>li').element_by(have.exact_text('c')).double_click()
-    ss('#todo-list>li').element_by(have.css_class('editing')).element('.edit')\
-        .type(' to be canceled').press_escape()
+    app.cancel_editing('c', 'c to be canceled')
 
-    ss('#todo-list>li').element_by(have.exact_text('c')).hover()\
-        .element('.destroy').click()
-    ss('#todo-list>li').should(have.exact_texts('a'))
+    app.delete('c')
+    app.should_have('a')
